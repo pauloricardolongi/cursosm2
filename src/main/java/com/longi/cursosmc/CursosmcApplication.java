e1c988bbf8a2e2d0d588069826b989e64f4b5fc2
pauloricardolongi <pauloricardolongi@gmail.com>
2022-02-22 01:49:03 -0300

    Primeira versão

---
+++ src/main/java/com/longi/cursosmc/CursosmcApplication.java
@@ -0,0 +1,13 @@
+package com.longi.cursosmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/longi/cursosmc/CursosmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longi.cursosmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
