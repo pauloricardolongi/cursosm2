f03c648376dc818d2dc359eeba47160e79f2e8e6
pauloricardolongi <pauloricardolongi@gmail.com>
2022-02-23 03:02:18 -0300

    Na classe Principal fazer o Produto reconhecer suas Categorias

--- src/main/java/com/longi/cursosmc/CursosmcApplication.java
+++ src/main/java/com/longi/cursosmc/CursosmcApplication.java
@@ -33,6 +33,10 @@ public class CursosmcApplication implements CommandLineRunner{
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
 	}
