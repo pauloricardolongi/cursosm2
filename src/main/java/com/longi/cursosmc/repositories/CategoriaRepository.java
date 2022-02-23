aacdd04ab4ddabc4c8ea94f27e0bf796046bde68
pauloricardolongi <pauloricardolongi@gmail.com>
2022-02-23 00:23:50 -0300

    Criando Operação de instanciação e inserindo Categoria no Banco

--- src/main/java/com/longi/cursosmc/repositories/CategoriaRepository.java
+++ src/main/java/com/longi/cursosmc/repositories/CategoriaRepository.java
@@ -1,6 +1,8 @@
 package com.longi.cursosmc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+
 import org.springframework.stereotype.Repository;
 
 import com.longi.cursosmc.dominio.Categoria;
