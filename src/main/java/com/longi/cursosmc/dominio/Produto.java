c651fb582ea0c5152582491e99d9d64a76915d44
pauloricardolongi <pauloricardolongi@gmail.com>
2022-02-23 01:17:08 -0300

    Mapeamento basico classe Produto

--- src/main/java/com/longi/cursosmc/dominio/Produto.java
+++ src/main/java/com/longi/cursosmc/dominio/Produto.java
@@ -5,10 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produto implements Serializable {	
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private Double preco;
