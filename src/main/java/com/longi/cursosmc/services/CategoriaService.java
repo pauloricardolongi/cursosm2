75633751dfb7ef7b3b66d1a3ac4918b76e42988b
pauloricardolongi <pauloricardolongi@gmail.com>
2022-02-24 04:45:19 -0300

    Ajuste no endpoint / catetorias/ {id}

--- src/main/java/com/longi/cursosmc/services/CategoriaService.java
+++ src/main/java/com/longi/cursosmc/services/CategoriaService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.longi.cursosmc.dominio.Categoria;
 import com.longi.cursosmc.repositories.CategoriaRepository;
+import com.longi.cursosmc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,8 +18,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 		
 	}
 
-}
+
