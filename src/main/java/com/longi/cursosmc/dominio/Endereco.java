8c66c1eea0f1745a084f2b04c909da88bcded5f0
pauloricardolongi <pauloricardolongi@gmail.com>
2022-02-26 03:13:00 -0300

    Anotacoes Cliente, Endereco e Telefone

--- src/main/java/com/longi/cursosmc/dominio/Endereco.java
+++ src/main/java/com/longi/cursosmc/dominio/Endereco.java
@@ -3,10 +3,21 @@ package com.longi.cursosmc.dominio;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+
 public class Endereco implements Serializable {	
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String endereco;
 	private String numero;
@@ -14,7 +25,12 @@ public class Endereco implements Serializable {
 	private String bairro;
 	private String cep;
 	
+	@ManyToOne
+	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
+	
+	@ManyToOne
+	@JoinColumn(name="cidade_id")
 	private Cidade cidade;
 	
 	public Endereco() {
